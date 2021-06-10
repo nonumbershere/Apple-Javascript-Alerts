f47c7a0a1294b2045971b60c6f9b2a30f28b88b4
Lapide <64395933+nonumbershere@users.noreply.github.com>
2021-06-09 22:49:28 -0400

    Update alerts.script.js

--- alerts.script.js
+++ alerts.script.js
@@ -7,12 +7,33 @@ defaultButton: "Ok"
 });
 return t;
 }
+
 function alert(message) {
 var application = Application.currentApplication();
 application.includeStandardAdditions = true
 var t = application.displayDialog(message, {
 buttons: ["Ok"],
-defaultButton: "Ok"
+defaultButton: "Ok",
 });
 return t;
+}
+function prompt(message, ans) {
+var application = Application.currentApplication();
+application.includeStandardAdditions = true
+if (ans) {var t = application.displayDialog(message, {
+buttons: ["Ok"],
+defaultButton: "Ok",
+defaultAnswer:ans
+});
+return t;
+} else {
+var application = Application.currentApplication();
+application.includeStandardAdditions = true
+var t = application.displayDialog(message, {
+buttons: ["Ok"],
+defaultButton: "Ok",
+defaultAnswer:""
+});
+return t;
+}
 }
