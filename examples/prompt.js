a85372a062bb6daa57836dcc011456c122dee7e0
Lapide <64395933+nonumbershere@users.noreply.github.com>
2021-06-09 22:51:19 -0400

    Create prompt.js

---
+++ examples/prompt.js
@@ -0,0 +1,42 @@
+function confirm(message) {
+var application = Application.currentApplication();
+application.includeStandardAdditions = true
+var t = application.displayDialog(message, {
+buttons: ["Ok", "Cancel"],
+defaultButton: "Ok"
+});
+return t;
+}
+
+function alert(message) {
+var application = Application.currentApplication();
+application.includeStandardAdditions = true
+var t = application.displayDialog(message, {
+buttons: ["Ok"],
+defaultButton: "Ok",
+});
+return t;
+}
+function prompt(message, ans) {
+var application = Application.currentApplication();
+application.includeStandardAdditions = true
+if (ans) {var t = application.displayDialog(message, {
+buttons: ["Ok"],
+defaultButton: "Ok",
+defaultAnswer:ans
+});
+return t;
+} else {
+var application = Application.currentApplication();
+application.includeStandardAdditions = true
+var t = application.displayDialog(message, {
+buttons: ["Ok"],
+defaultButton: "Ok",
+defaultAnswer:""
+});
+return t;
+}
+}
+
+var promptBox = prompt("Your Name", 'Name');
+alert("Hello "+promptBox.textReturned+"!");
